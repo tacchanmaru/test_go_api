5446a770ad3789949e5c2e4ef4f5e9adc9b3947d
Tatsuya <tatsu.mase@icloud.com>
2024-05-16 00:19:44 +0900

    create handlers

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
+	"github.com/tacchanmaru/test_go_api/handlers"
 )
 
 func main(){
-	helloHandler := func(w http.ResponseWriter, req *http.Request){
-		io.WriteString(w, "Hello, world!\n")
-	}
 
-	http.HandleFunc("/", helloHandler)
-
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+  http.HandleFunc("/comment", handlers.PostCommentHandler)
+	
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
