e34c2931249698f1d807b94780a87d4fd07213a0
Tatsuya <tatsu.mase@icloud.com>
2024-05-16 15:15:07 +0900

    ルータをgorrila/muxに変えた

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tacchanmaru/test_go_api
 
 go 1.22.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,51 +8,27 @@ import (
 
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Hello, world!\n")
-	} else {
-		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Hello, world!\n")
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request){
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Article...\n")
-	} else {
-		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Article...\n")
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request){
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Article List\n")
-	} else {
-		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Article List\n")
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request){
-	if req.Method == http.MethodGet {
-		articleID := 1
-		resString := fmt.Sprintf(("Article No.%d\n"), articleID)
-		io.WriteString(w, resString)
-	} else {
-		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
-	}
+	articleID := 1
+	resString := fmt.Sprintf(("Article No.%d\n"), articleID)
+	io.WriteString(w, resString)
 }
 
 func PostNiceHandler(w http.ResponseWriter, req *http.Request){
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Nice...\n")
-	} else {
-		http.Error(w, "invalid Method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Nice...\n")
 }
 
 func PostCommentHandler(w http.ResponseWriter, req *http.Request){
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Comment...\n")
-	} else {
-		http.Error(w, "invalid Method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Comment...\n")
 }
